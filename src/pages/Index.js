77ca0cfb6baef2f8779ec0b28d1bfc14ee2c69d2
Jonathan1295-09 <jcasasola955@gmail.com>
2023-09-28 00:36:51 -0500

    pages/Index.js edit

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,21 @@
+import Post from '../components/Post';
+import { useLoaderData } from 'react-router-dom';
+
 function Index (props){
-    return <div>Index</div>
+    const personalbests = useLoaderData()
+    return<>
+    <div style={{textAlign: "center"}}>
+        <h2>Create a personalbest</h2>
+            <Form method="Post" action="/create">
+                <input type='text' name='subject' placeholder='Cardio'/>
+                <input type='text' name='subject' placeholder='Bench Press'/>
+                <input type='text' name='subject' placeholder='Deadlift'/>
+                <input type='text' name='subject' placeholder='Squat'/>
+                <input type='text' name='subject' placeholder='Shoulder Press'/>
+            </Form>
+    </div>
+    {personalbests.map((personalbest) => <Post key={personalbest.id} post={personalbest}/>)}
+    </>
 }
 
 export default Index;

82db178b58d34e5494b0ccc76d4652cdfa72c04b
Jonathan1295-09 <jcasasola955@gmail.com>
2023-09-28 00:26:55 -0500

    App.js, Router.js, url.js fixed

--- src/router.js
+++ src/router.js
@@ -1,14 +1,15 @@
-import {createBrowserRouter, createdRoutesFromElement, Route} from 'react-router-dom';
+import {createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom';
 import App from "./App";
 import Index from './pages/Index';
 import Show from './pages/Show';
+import { indexLoader, showLoader } from './loaders';
 
 
-const router = createBrowserRouter(createdRoutesFromElement(
+const router = createBrowserRouter(createRoutesFromElements(
     <>
         <Route path="/" element={<App/>}>
-            <Route path="" element={<Index/>} />
-            <Route path=":id" element={<Show/>} />
+            <Route path="" element={<Index/>} loader={indexLoader}/>
+            <Route path=":id" element={<Show/>} loader={showLoader}/>
             <Route path="create" />
             <Route path="update/:id" />
             <Route path="delete/:id" />
--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
-const url = "https://personalbest.onrender.com"
+const url = "https://personalbest.onrender.com/personalbest"
 
 export default url
